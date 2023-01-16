c58a81a3397e29ccfaf703cd7209579f2849d6d3
viniciustixu <vinicipkt@live.com>
2023-01-16 15:09:01 -0300

    Objects - função dentro de objeto

---
+++ Obejects/ex002 - função dentro de objeto/index.js
@@ -0,0 +1,9 @@
+//Meu objeto
+let pessoa = {
+    digaOi: function(name) {
+        return `Olá ${name}`
+    }
+}
+
+//chamando a função dentro do meu objeto
+console.log(pessoa.digaOi('Vinicius'))
