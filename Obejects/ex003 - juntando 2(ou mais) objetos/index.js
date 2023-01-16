02551dcda7715dea6cc82e44ab2db6c4c6a505dd
viniciustixu <vinicipkt@live.com>
2023-01-16 18:01:37 -0300

    Objeto - juntando 2 ou mais objetos

--- Obejects/ex003 - juntando 2(ou mais) objetos/index.js
+++ Obejects/ex003 - juntando 2(ou mais) objetos/index.js
@@ -13,11 +13,30 @@ let outrosDados = {
 // Para juntar os 2 em uma variavel nova ficaria:
 let novoUsuario = Object.assign({}, pessoa, outrosDados)
 
-// Se quiser juntar os 2 mas não em uma nova variavel ficaria:
+
+// Se quiser juntar os 2 valores em 'pessoa' ficaria: (Evitar usar esse de acordo com o professor)
 Object.assign(pessoa, outrosDados)
 
 
+//Também há uma terceira opção:
+let novoUsuario2 = {
+    ...pessoa,          // (...) Isso se chama spread, ele imprime os valores que estão dentro do objeto
+    ...outrosDados,     // Em vez de ficar novoUsuario2.pessoa.nome, vou poder usar apenas novoUsuario2.nome
+    
+    ...{
+        refri: 'coca cola',   // Este exemplo já está gerando valores novos dentro do objeto novoUsuario2
+        cafe: 'padre vitor',
+    }
+    
+/*  Ou eu poderia simplesmente abreviar da seguinte maneira:
+    headset: 'G pro X',  
+    teclado: 'blackWidow',
+*/
+}
 
 
 
-console.log(novoUsuario)
+
+
+
+console.log(novoUsuario2)
