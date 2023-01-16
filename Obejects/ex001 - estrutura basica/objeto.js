71bbb10b80fbec6ecf1b1be447732a13aecbf3d9
viniciustixu <vinicipkt@live.com>
2023-01-16 11:43:32 -0300

    Objects - estrutura básica

---
+++ Obejects/ex001 - estrutura basica/objeto.js
@@ -0,0 +1,10 @@
+//Meu objeto
+let pessoa = {
+    nome: 'Vinicius',
+    idade: '27',
+    linguagens: 'javascript',
+}
+
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+console.log(pessoa.linguagens)
